815908123ae7dfcad4a26df45f0d5e6e32c8d03f
Jack Goldsmith <jackgoldsmith4@gmail.com>
2020-04-04 00:42:50 -0400

    wip question state

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Paper, Divider, TextField, Checkbox } from '@material-ui/core';
+import { Grid, Paper, Divider, TextField, Radio } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const classes = makeStyles(theme => ({
@@ -9,7 +9,7 @@ const classes = makeStyles(theme => ({
     color: theme.palette.text.secondary,
   },
 }));
-
+//TODO make question and answers and radio button REQUIRED
 class Question extends React.Component {
     constructor(props) {
         super(props);
@@ -23,6 +23,33 @@ class Question extends React.Component {
         }
     }
 
+    handleQuestionChange(newQuestion) {
+        this.setState({question: newQuestion})
+    }
+
+    handleAnswerChange(newAnswer, answerNumber) {
+        switch (answerNumber) {
+            case 1:
+                this.setState({answer1: e.target.value});
+                break;
+            case 2:
+                this.setState({answer2: e.target.value});
+                break;
+            case 3:
+                this.setState({answer3: e.target.value});
+                break;
+            case 4:
+                this.setState({answer4: e.target.value});
+                break;
+            default:
+                throw new Error(); //TODO document
+        }
+    }
+
+    handleRadioChange(correctAnswerNum) {
+        this.setState({correctAnswer: correctAnswerNum});
+    }
+
     render() {
         return (
             <React.Fragment>
@@ -33,7 +60,7 @@ class Question extends React.Component {
                                 label='Question:'
                                 fullWidth
                                 variant='outlined'
-                                onChange={(e) => this.setState({question: e.target.value})}
+                                onChange={e => this.handleQuestionChange(e.target.value)}
                             />
                         </Paper>
                      </Grid>
@@ -43,9 +70,8 @@ class Question extends React.Component {
                                 label='Answer Choice 1:'
                                 fullWidth
                                 variant='outlined'
-                                onChange={(e) => this.setState({answer1: e.target.value})}
+                                onChange={(e) => this.handleAnswerChange(e.target.value, 1)}
                             />
-                            <Checkbox color='primary'/>
                          </Paper>
                      </Grid>
                      <Grid item xs={6} sm={6}>
@@ -54,9 +80,8 @@ class Question extends React.Component {
                                 label='Answer Choice 2:'
                                 fullWidth
                                 variant='outlined'
-                                onChange={(e) => this.setState({answer2: e.target.value})}
+                                onChange={(e) => this.handleAnswerChange(e.target.value, 2)}
                             />
-                            <Checkbox color='primary'/>
                          </Paper>
                      </Grid>
                      <Grid item xs={6} sm={6}>
@@ -65,9 +90,8 @@ class Question extends React.Component {
                                 label='Answer Choice 3:'
                                 fullWidth
                                 variant='outlined'
-                                onChange={(e) => this.setState({answer3: e.target.value})}
+                                onChange={(e) => this.handleAnswerChange(e.target.value, 3)}
                             />
-                            <Checkbox color='primary'/>
                          </Paper>
                      </Grid>
                      <Grid item xs={6} sm={6}>
@@ -76,11 +100,19 @@ class Question extends React.Component {
                                 label='Answer Choice 4:'
                                 fullWidth
                                 variant='outlined'
-                                onChange={(e) => this.setState({answer4: e.target.value})}
+                                onChange={(e) => this.handleAnswerChange(e.target.value, 4)}
                             />
-                            <Checkbox color='primary'/>
                          </Paper>
                      </Grid>
+                     <Grid item xs={12}>
+                         <Paper className={classes.paper}>
+                             <Radio
+                                checked={this.state.correctAnswer === 1}
+                                onChange={this.handleRadioChange(1)}
+                                value='1'
+                             />
+                         </Paper>
+                      </Grid>
                  </Grid>
 
                 <Grid item xs={12} />
