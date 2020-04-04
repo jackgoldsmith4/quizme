a550765869f5be729dae56bfe1bf79b044c1b62e
Jack Goldsmith <jackgoldsmith4@gmail.com>
2020-04-04 10:59:26 -0400

    remove paper styling

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,14 +2,6 @@ import React from 'react';
 import { Grid, Paper, Divider, TextField, Radio, RadioGroup, FormControl, FormControlLabel, FormLabel } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
-const styles = makeStyles(theme => ({
-  paper: {
-      padding: theme.spacing(2),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-  },
-}));
-
 //TODO make question and answers and radio button REQUIRED FIELDS
 class Question extends React.Component {
     constructor(props) {
@@ -60,7 +52,7 @@ class Question extends React.Component {
             <React.Fragment>
                  <Grid container alignItems='center'>
                      <Grid item xs={12}>
-                        <Paper className={styles.paper}>
+                        <Paper>
                             <TextField
                                 label='Question:'
                                 fullWidth
@@ -71,7 +63,7 @@ class Question extends React.Component {
                         </Paper>
                      </Grid>
                      <Grid item xs={6} sm={6}>
-                         <Paper className={styles.paper}>
+                         <Paper>
                             <TextField
                                 label='Answer Choice 1:'
                                 fullWidth
@@ -82,7 +74,7 @@ class Question extends React.Component {
                          </Paper>
                      </Grid>
                      <Grid item xs={6} sm={6}>
-                         <Paper className={styles.paper}>
+                         <Paper>
                             <TextField
                                 label='Answer Choice 2:'
                                 fullWidth
@@ -93,7 +85,7 @@ class Question extends React.Component {
                          </Paper>
                      </Grid>
                      <Grid item xs={6} sm={6}>
-                         <Paper className={styles.paper}>
+                         <Paper>
                             <TextField
                                 label='Answer Choice 3:'
                                 fullWidth
@@ -104,7 +96,7 @@ class Question extends React.Component {
                          </Paper>
                      </Grid>
                      <Grid item xs={6} sm={6}>
-                         <Paper className={styles.paper}>
+                         <Paper>
                             <TextField
                                 label='Answer Choice 4:'
                                 fullWidth
@@ -115,7 +107,7 @@ class Question extends React.Component {
                          </Paper>
                      </Grid>
                      <Grid item xs={12}>
-                         <Paper className={styles.paper}>
+                         <Paper>
                              <FormControl variant='outlined' fullWidth required={true}>
                                 <RadioGroup
                                     row
